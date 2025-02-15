cd4f492aceddf29c4bbe6170b861b29926f36df5
WEKOOO <abbrorw@gmail.com>
2025-02-15 20:37:49 +0700

    revisi

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 # Load data dengan error handling yang lebih robust
