21f1d9ffee1fc53b8e8db9f1c957f1c3275303f6
WEKOOO <abbrorw@gmail.com>
2025-02-16 12:25:51 +0700

    add..

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -8,8 +8,8 @@ import seaborn as sns
 # Load dataset
 @st.cache_data
 def load_data():
-    day_df = pd.read_csv('../data/day.csv')
-    hour_df = pd.read_csv('../data/hour.csv')
+    day_df = pd.read_csv('data/day.csv')
+    hour_df = pd.read_csv('data/hour.csv')
     return day_df, hour_df
 
 day_df, hour_df = load_data()
